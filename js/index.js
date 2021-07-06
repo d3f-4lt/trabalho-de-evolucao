ad994f6b3207241d944baf7ba66ac168769a53e4
= <=>
2021-07-06 00:56:13 -0300

    correções e ajustes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 async function animar() {
 	var divs = document.getElementsByTagName('div');
 	for (let i = 0; i < divs.length; i++) {
-		await delay(200);
+		await delay(100);
 		divs[i].classList.add('animar');
 	}
 }
